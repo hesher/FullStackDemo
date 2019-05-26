ee47d657f9d851fdb27d26e83f515057b8e36d92
Ben Katz <bkatz@fb.com>
2019-05-26 20:36:30 +0100

    separate todobutton(2)

--- src/client/Todo/TodoButton.js
+++ src/client/Todo/TodoButton.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './TodoButton.css';
+import PropTypes from 'prop-types';
 
 
-const TodoButton = ({ children, onClick, disabled=false }) => (<button className="Todos_Action_button" type="button" onClick={onClick}
-  disabled={disabled}
+const TodoButton = ({ children, onClick, disabled = false }) => (<button className="Todos_Action_button" type="button" onClick={onClick}
+    disabled={disabled}
 >
-  {children}
+    {children}
 </button>);
+
+
+Todo.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool
+}

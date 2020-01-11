d0c8ca3845549810d20971a25db238b8216ae0e3
Ben Katz <ben.katz@gmail.com>
2020-01-11 14:56:24 +0000

    add fit

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,7 @@ const {promisify} = require('util');
 const app = express();
 const FILE_PATH = './todos.json';
 const gpxParse = require('gpx-parse');
+const FitParser = require('fit-file-parser').default;
 
 const readTodosFile = () => {
   return promisify(fs.readFile)(FILE_PATH);
@@ -28,14 +29,43 @@ app.use(bodyParser.json());
 
 const path = './test2.gpx';
 app.get('/api/gpx', (req, res, next) => {
+  if (!fs.existsSync(path)) {
+    next(`The file ${path} doesn't exist`);
+  }
+
   gpxParse.parseGpxFromFile(path, (error, data) => {
-    if (!fs.existsSync(path)) {
-      next(`The file ${path} doesn't exist`);
-    } else if (error) {
+    if (error) {
       next(`error: ${error}`);
     } else {
       res.send(`success, tracks number=${data.tracks.length}`);
     }
+  });
+});
+app.get('/api/fit', async (req, res, next) => {
+  const fitPath = './test.fit';
+  if (!fs.existsSync(fitPath)) {
+    next(`The file ${fitPath} doesn't exist`);
+  }
+  const fitParser = new FitParser({
+    force: true,
+    speedUnit: 'km/h',
+    lengthUnit: 'km',
+    temperatureUnit: 'celsius',
+    elapsedRecordField: true,
+    mode: 'cascade'
+  });
+
+  return fs.readFile(fitPath, (err, content) => {
+    return fitParser.parse(content, (error, data) => {
+      // Handle result of parse method
+      if (error) {
+        console.log('111111');
+        next(error);
+      } else {
+        console.log('2222');
+        res.send(JSON.stringify(data, null, 2));
+      }
+    });
   });
 });
 
